770cc6cb198369d2545240a3c8d00bc163d16389
bea <beatriztattooer@gmail.com>
2022-07-21 12:13:19 -0300

    💄 Menu
    - fix some issues at mobile

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -3,7 +3,7 @@
         <TitleImg :imgPath="title" />
 
         <div class="menu__filter">
-            <span class="menu__filter-btn" v-for="filter in filters" :key="filter">{{ filter }}</span>
+            <span class="menu__filter__btn" v-for="filter in filters" :key="filter">{{ filter }}</span>
         </div>
 
         <div class="menu__items">
@@ -23,7 +23,7 @@
 </template>
 
 <script>
-import TitleImg from './TitleImg.vue'
+import TitleImg from './Titles/TitleImg.vue'
 import titleSrc from '@/assets/img/menu.svg'
 
 export default {
@@ -31,31 +31,35 @@ export default {
     data(){
         return{
             title: titleSrc,
-            filters: ['Simples', 'Especiais', 'Vegetarianos', 'Todos'],
+            filters: ['Simples', 'Especiais', 'Vegetarianos', 'Combos', 'Todos'],
             menuFood: [
                 {
                     thumb: 'src/assets/img/food/salsicha-da-casa.svg',
                     name: 'Salsicha da Casa',
                     description: 'Pão, salsicha, maionese especial, mostarda, catchup, pepino japonês, tomate cereja e cebola',
-                    price: 'R$ 10,00'
+                    price: 'R$ 10,00',
+                    category: 'simples'
                 },
                 {
                     thumb: 'src/assets/img/food/bidu-bidu.svg',
                     name: 'Bidu Bidu',
-                    description: 'Pão, salsicha, maionese especial, mostarda, catchup, vinagrete',
-                    price: 'R$ 10,00'
+                    description: 'Pão, salsicha veggie, maionese especial, mostarda, catchup, vinagrete',
+                    price: 'R$ 10,00',
+                    category: 'vegetariano'
                 },
                 {
                     thumb: 'src/assets/img/food/hotdogson.svg',
                     name: 'Hotdogson Combo',
                     description: 'O clássico com 250g de batata frita e aquela coquinha gelada de 350ml!',
-                    price: 'R$ 10,00'
+                    price: 'R$ 10,00',
+                    category: 'combo'
                 },
                 {
                     thumb: 'src/assets/img/food/monstro-da-dungeon.svg',
                     name: 'Monstro da Dungeon',
                     description: 'Pãozão com salsicha, maionese especial, bacon, pepino, muito queijo e cebola roxa',
-                    price: 'R$ 10,00'
+                    price: 'R$ 10,00',
+                    category: 'especial'
                 }
             ]
         }
@@ -64,57 +68,66 @@ export default {
 </script>
 
 <style>
-.menu__filter{
-    max-width: 600px;
+.menu__filter {
+    max-width: 800px;
     margin: 0 auto 2rem;
+
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
 }
-.menu__filter-btn{
+.menu__filter__btn {
     padding: 0.7rem 1rem;
     margin: 0.5em;
+
     text-align: center;
     font-size: 1rem;
+    color: #4D4D4D;
+
     border-radius: 0.2rem;
     background-color: #FFC1A6;
-    color: #4D4D4D;
 }
 
-.menu__items{
+.menu__items {
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(600px, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    gap: 2em;
 }
-.menu__item{
+.menu__item {
     display: block;
     max-width: 300px;
-    margin: 1rem auto;
+    margin: auto;
 }
-.menu__item__img{
+.menu__item__img {
     max-width: 270px;
     max-height: 270px;
 }
-.menu__item__text-block{
+.menu__item__text-block {
     margin: 1rem;
+    display: grid;
 }
-.menu__item__name{
+.menu__item__name {
     font-size: 1.5rem;
     font-weight: 700;
 }
-.menu__item__description{
+.menu__item__description {
     font-weight: lighter;
 }
-.menu__item__btn{
+.menu__item__btn {
     padding: 0.7rem 1rem;
-    position: absolute;
-    bottom: 0;
+
     text-align: center;
     font-size: 1rem;
-    border-radius: 0.2rem;
-    background-color: #FFC1A6;
     color: #4D4D4D;
+
+    border: none;
+    border-radius: 0.5em;
+    background-color: #FFC1A6;
 }
 @media screen and (min-width: 1024px) {
-    .menu__item{
+    .menu__items {
+        grid-template-columns: repeat(auto-fit, minmax(600px, 1fr));
+    }
+    .menu__item {
         display: flex;
         max-width: 560px;
         margin: 1rem auto;
